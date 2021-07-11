bf05d90ac62dc257dcd232b512bf4b39d1c576d2
Albert-Einstein-007 <86305892+Albert-Einstein-007@users.noreply.github.com>
2021-07-11 11:15:18 +0530

    Create view table fire.js

---
+++ view table fire.js
@@ -0,0 +1,102 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyCXyLelGhJD2F4IpUXouJp8GZeq8rZKR0U",
+
+    authDomain: "love-calc-abin.firebaseapp.com",
+
+    databaseURL: "https://love-calc-abin-default-rtdb.firebaseio.com",
+
+    projectId: "love-calc-abin",
+
+    storageBucket: "love-calc-abin.appspot.com",
+
+    messagingSenderId: "530983451208",
+
+    appId: "1:530983451208:web:1696563e4e8145a171367d"
+
+  };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+window.onload = get()
+
+function get(){
+
+var ref = firebase.database().ref("users");
+
+ref.once("value")
+
+  .then(function(snapshot) {
+
+    let number = snapshot.numChildren();
+
+    console.log(number)    
+
+    
+
+    
+
+            
+
+    for (let i = 1; i < number+1; i++) {
+
+ firebase.database().ref("users/"+i).on('value', function(snapshot){ 
+
+ var data = snapshot.val() 
+
+var dp = data.UName;
+
+console.log(i)
+
+var dn= data.LName;
+
+ console.log(i)
+
+//var table = document.createElement("table")
+
+var tr = document.createElement("tr"); 
+
+var td1 = document.createElement("td"); 
+
+var td2 = document.createElement("td"); 
+
+            var text1 =  
+
+                document.createTextNode(dp); 
+
+                var text2 =  
+
+                document.createTextNode(dn); 
+
+ 
+
+      //  table.appendChild(tr);
+
+ 
+
+const element = document.getElementById("table");
+
+            tr.appendChild(td1); 
+
+            tr.appendChild(td2);
+
+        
+
+            td1.appendChild(text1)
+
+            td2.appendChild(text2)
+
+            
+
+element.appendChild(tr);
+
+});
+
+}
+
+});
+
+}
+
